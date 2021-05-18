b460d428762ed442cdd62fb20bf6f26272b4f582
Rezaul Islam <rezaul.islam@tekarsh.com>
2021-05-18 18:48:31 +0600

    init

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+ import React from 'react';
+import { Provider } from 'react-redux';
+import RootNavigator from './src/navigations/RootNavigator';
+import { store } from './src/redux/store';
+
+ const App = () => {
+  
+   return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+   );
+ };
+
+ export default App;
